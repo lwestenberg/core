0b9d7e46b4687f669a7c0e80c8785814ae5ed853
Lauren Westenberg <17823076+lwestenberg@users.noreply.github.com>
2022-10-25 20:11:58 +0000

    create basic first bold smart lock integration

--- homeassistant/components/bold/const.py
+++ homeassistant/components/bold/const.py
@@ -1,5 +1,17 @@
 """Constants for the Bold integration."""
+from homeassistant.const import Platform
 
 DOMAIN = "bold"
+
+PLATFORMS = [
+    Platform.LOCK,
+]
+
 OAUTH2_AUTHORIZE = "https://auth.boldsmartlock.com/"
 OAUTH2_TOKEN = "https://api.boldsmartlock.com/v2/oauth/token"
+
+CONF_ACTUAL_FIRMWARE_VERSION = "actualFirmwareVersion"
+CONF_BATTERY_LAST_MEASUREMENT = "batteryLastMeasurement"
+CONF_BATTERY_LEVEL = "batteryLevel"
+CONF_MAKE = "make"
+CONF_PERMISSION_REMOTE_ACTIVATE = "permissionRemoteActivate"
---
+++ homeassistant/components/bold/coordinator.py
@@ -0,0 +1,32 @@
+"""Coordinator to fetch the data."""
+from datetime import timedelta
+import logging
+
+import async_timeout
+from bold_smart_lock.bold_smart_lock import BoldSmartLock
+
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class BoldCoordinator(DataUpdateCoordinator):
+    """Bold data coordinator."""
+
+    def __init__(self, hass, bold: BoldSmartLock):
+        """Initialize Bold coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Bold",
+            update_interval=timedelta(seconds=60),
+        )
+        self.bold = bold
+
+    async def _async_update_data(self):
+        """Fetch data from Bold Smart Lock API."""
+        try:
+            async with async_timeout.timeout(10):
+                return await self.bold.get_device_permissions()
+        except Exception as exc:
+            raise UpdateFailed("Error communicating with API") from exc
